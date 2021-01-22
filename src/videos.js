f09891bd2f3a7ffb2f474ae784df10395a331f7e
Einar Pálsson <epp5@hi.is>
2021-01-22 12:52:22 +0000

    testing

--- src/videos.js
+++ src/videos.js
@@ -1,1 +1,26 @@
-console.log("hi");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Bjarni');
+  console.log(res.send);
+});
+
+app.use('/static', express.static('public'));
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request');
+});
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request from /user');
+});
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
